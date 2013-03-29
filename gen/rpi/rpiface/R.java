1af09678536d8b6ca602b004f9c27b28c7c7b148
Jalgonm <jalgonm@gmail.com>
2013-03-29 13:00:58 +0100

    Actualizados iconos

--- gen/rpi/rpiface/R.java
+++ gen/rpi/rpiface/R.java
@@ -14,8 +14,7 @@ public final class R {
         public static final int fondo=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int launcher=0x7f020003;
-        public static final int logo=0x7f020004;
+        public static final int logo=0x7f020003;
     }
     public static final class id {
         public static final int button_angry=0x7f070006;
