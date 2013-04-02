c5e642a700bef6808239102bf2448bc50a0bde1a
Jalgonm <jalgonm@gmail.com>
2013-04-02 23:37:02 +0200

    Añadidos iconos a los botones del menu

--- gen/rpi/rpiface/R.java
+++ gen/rpi/rpiface/R.java
@@ -11,10 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int fondo=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int logo=0x7f020003;
+        public static final int dialog=0x7f020000;
+        public static final int fondo=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_menu_contact=0x7f020004;
+        public static final int ic_menu_mark=0x7f020005;
+        public static final int ic_menu_start_conversation=0x7f020006;
+        public static final int logo=0x7f020007;
     }
     public static final class id {
         public static final int button_angry=0x7f070006;
